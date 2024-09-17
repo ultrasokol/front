2606898ab79fa61e83ec035f19756ae3f9e0b3ba
Kireenkov22rus) <d.kireenkov32rus@yandex.ru>
2024-09-17 21:34:19 +0300

    first commit123

--- scripts/copy-script.js
+++ scripts/copy-script.js
@@ -1,21 +1,12 @@
 import { copyFileSync } from 'fs'
-import pkg from 'fs-extra'
+
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
-const { copySync } = pkg
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const source = join(__dirname, '..', '.htaccess')
 const destination = join(__dirname, '..', 'public', '.htaccess')
 
-const mail = join(__dirname, '..', 'mail.php')
-const mail2 = join(__dirname, '..', 'public', 'mail.php')
-
-const phpmailer = join(__dirname, '..', 'phpmailer')
-const phpmailerPublic = join(__dirname, '..', 'public', 'phpmailer')
-
 copyFileSync(source, destination)
-copyFileSync(mail, mail2)
-copySync(phpmailer, phpmailerPublic)
